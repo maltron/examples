aa069f1665021d9a7f7f6edcddbec651ce765e46
Mauricio Leal <maltron@gmail.com>
2015-11-02 20:47:53 -0200

    Testing for Ticket selection

--- java/event-registration/event/src/main/java/net/nortlam/event/registration/mbean/RegistrationController.java
+++ java/event-registration/event/src/main/java/net/nortlam/event/registration/mbean/RegistrationController.java
@@ -5,7 +5,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.ViewScoped;
+import javax.faces.bean.RequestScoped;
 import javax.faces.event.ActionEvent;
 import net.nortlam.event.registration.entity.Attendee;
 import net.nortlam.event.registration.entity.Event;
@@ -22,7 +22,7 @@ import static net.nortlam.event.registration.util.Extraction.extractEdition;
  *
  * @author Mauricio "Maltron" Leal <maltron@gmail.com> */
 @ManagedBean(name="register")
-@ViewScoped
+@RequestScoped
 public class RegistrationController extends EventRegistrationCommonController
                                                         implements Serializable {
     
@@ -63,6 +63,7 @@ public class RegistrationController extends EventRegistrationCommonController
     public String getDesignationEdition() { return null; } // NOT USED
     
     public Event getEvent() {
+        if(event == null) LOG.log(Level.SEVERE, "### getEvent() #1 event is NULL");
         return event; 
     }
     
@@ -71,6 +72,7 @@ public class RegistrationController extends EventRegistrationCommonController
     }
     
     public Organizer getOrganizer() {
+        if(event == null) LOG.log(Level.SEVERE, "### getEvent() #2 event is NULL");
         try {
             if(event.getOrganizer() > 0)
                 return service.requestOrganizerByID(hostOrganizerService(), 
@@ -119,7 +121,13 @@ public class RegistrationController extends EventRegistrationCommonController
         return "/event_registration.xhtml";
     }
     
-    public void register(ActionEvent event) {
+    public String register() {
+        // At least, one ticket must be selected in order to perform a sell
+        if(!isTicketSelected()) {
+            error("You need to select a quantity in order to register");
+            return null;
+        }
         
+        return "ticket2";
     }
 }
