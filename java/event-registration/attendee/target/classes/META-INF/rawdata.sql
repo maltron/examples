36c2d0b25b292fd892cd8dee8fa9db9fe8670876
Mauricio Leal <maltron@gmail.com>
2015-11-17 10:14:32 -0200

    Removed undesirable files

--- java/event-registration/attendee/target/classes/META-INF/rawdata.sql
+++
@@ -1,1 +0,0 @@
-insert into ATTENDEE(EMAIL, FIRST_NAME, LAST_NAME, PASSWORD) values('maltron@gmail.com', 'Mauricio', 'Leal', 'pmWkWSBCL51Bfkhn79xPuKBKHz//H6B+mY6G9/eieuM=');
--- java/event-registration/organizer/target/classes/META-INF/rawdata.sql
+++
@@ -1,2 +0,0 @@
-## Organizer 
-insert into ORGANIZER(EMAIL, FIRST_NAME, LAST_NAME, PASSWORD) values('maltron@gmail.com', 'Mauricio', 'Leal', 'pmWkWSBCL51Bfkhn79xPuKBKHz//H6B+mY6G9/eieuM=');
